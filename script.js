8aa45759ee28aae58ba255629f768ba196801292
Christiana-Udoh17 <silaschristiana693@gmail.com>
2024-12-09 15:59:28 +0100

    Completed my AI poem

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-function jokeGenerator(response) {
-  let joke = document.querySelector("#joke");
-  joke.innerHTML = response.data.answer;
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 100,
+  });
 }
 
-let apiKey = "75ea6de5c64t62643f0of2d771bd9f16";
-let prompt = "Tell me a joke";
-let context = "Very funny jokes";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-axios.get(apiUrl).then(jokeGenerator);
+function generatePoem(event) {
+  event.preventDefault();
+  // let poem = document.querySelector("#poem");
+  // poem.innerHTML = "I love light";
+  let inputElement = document.querySelector("#title");
+  let inputInstruction = inputElement.value;
+  let apiKey = "75ea6de5c64t62643f0of2d771bd9f16";
+  let prompt = `UserInstructions:Generate a poem about ${inputInstruction}`;
+  let context = `Your a elegy poem writer. Generate a 7 line extremely  sorrowful poem about${inputInstruction}seperating each line with a<br/>in HTML. sign at the end  with the authors name  wrapped in <strong/> tag without any asteriks symbol.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemContainer = document.querySelector("#poem");
+  poemContainer.classList.remove("hidden");
+  poemContainer.innerHTML = `<div class="blink">⌛ Generating poem about ${inputInstruction}... </div>`;
 
-let jokeBtn = document.querySelector("button");
-jokeBtn.addEventListener("click", jokeGenerator);
+  axios.get(apiUrl).then(displayPoem);
+}
+
+let poemForm = document.querySelector("#poem-form");
+poemForm.addEventListener("submit", generatePoem);
